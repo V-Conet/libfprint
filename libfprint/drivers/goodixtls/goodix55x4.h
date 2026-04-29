5eb1213dfb695e494cc464f2d6423962c9836e24
V-Conet <46270998+V-Conet@users.noreply.github.com>
2026-04-29 22:29:52 +0800

    Add new VID/PID entry to id_table

--- libfprint/drivers/goodixtls/goodix55x4.h
+++ libfprint/drivers/goodixtls/goodix55x4.h
@@ -66,6 +66,7 @@ guint8 goodix_55x4_config[] = {
 static const FpIdEntry id_table[] = {
     {.vid = 0x27c6, .pid = 0x55b4},
     {.vid = 0x27c6, .pid = 0x55a4},
+    {.vid = 0x27c6, .pid = 0x5584},
     {.vid = 0, .pid = 0, .driver_data = 0},
 };
 
